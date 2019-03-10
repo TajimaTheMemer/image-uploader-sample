52cdd703403d11fccb043afe2cccdc5c4f43c9e8
tajima <tajima_taichi@cyberagent.co.jp>
2019-03-10 23:56:04 +0900

    save image with uuid name

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -8,13 +8,14 @@ import (
 func Upload(c *gin.Context) {
 	form, _ := c.MultipartForm()
 	files := form.File["file"]
+	uuid := c.PostForm["uuid"]
 
 	for _, file := range files {
-		err := c.SaveUploadedFile(file, "images/"+file.Filename)
+		err := c.SaveUploadedFile(file, "images/"+uuid+".png")
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "success!!"})
+	c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 }
